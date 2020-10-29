d39c0f3660c5778f568ed6a94dfaaf3ec33cdab2
dionatamsouza <dionatamsouza@yahoo.com.br>
2020-10-29 20:27:38 -0300

    Update Cidade.java

--- aula-tres/src/main/java/br/com/ifsul/core/model/Cidade.java
+++ aula-tres/src/main/java/br/com/ifsul/core/model/Cidade.java
@@ -6,10 +6,40 @@ public class Cidade {
     private String nome;
     private String sigla;
     private Estado estado;
-
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public String getSigla(){
+        return this.sigla;
+    }
+    
+    public Estado getEstado(){
+        return this.estado;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public void setSigla(String sigla){
+        this.sigla = sigla;
+    }
+    
+    public void setEstado(Estado estado){
+        this.estado = estado;
+    }
+    
     public Cidade(){}
 
     public Cidade(Estado estado) {
         this.estado = estado;
     }
+    
+    @Override
+    public String toString() {
+        return "Instancia: Cidade [%s]", this.nome;
+    }
+    
 }
